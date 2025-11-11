91eb33280ebedf3358726f30e05ac1091153c65c
PratikSabale <pratiksabale1428@gmail.com>
2025-11-11 13:07:10 +0530

    New Updates in Order Management

---
+++ src/pages/module/OrderManagement/OrderManagement.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import PagePath2 from "../../../components/uiComponent/PagePath2";
+import BreadCrumb from "../../../components/uiComponent/BreadCrumb";
+import { Box } from "@mui/material";
+import DataTable from "../../../components/uiComponent/DataTable";
+
+const OrderManagement = () => {
+  const data = [
+    {
+      orderId: "OI6798",
+      userName: "Dheeraj Jadhav",
+      userRole: "Staff",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Delivered",
+    },
+    {
+      orderId: "OI9743",
+      userName: "Raeesh Khan",
+      userRole: "User",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 2,
+      orderDate: "10/11/2025",
+      status: "Pending",
+    },
+    {
+      orderId: "OI9834",
+      userName: "Dheeraj Jadhav",
+      userRole: "User",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Returned",
+    },
+    {
+      orderId: "OI1234",
+      userName: "Sahai Meena",
+      userRole: "User",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Processing",
+    },
+    {
+      orderId: "OI7850",
+      userName: "Raeesh Khan",
+      userRole: "Staff",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Cancelled",
+    },
+    {
+      orderId: "OI6852",
+      userName: "Sahai Meena",
+      userRole: "User",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Returned",
+    },
+    {
+      orderId: "OI0689",
+      userName: "Dheeraj Jadhav",
+      userRole: "Staff",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Delivered",
+    },
+    {
+      orderId: "OI5548",
+      userName: "Raeesh Khan",
+      userRole: "User",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 1,
+      orderDate: "10/11/2025",
+      status: "Pending",
+    },
+    {
+      orderId: "OI6978",
+      userName: "Dheeraj Jadhav",
+      userRole: "User",
+      items: ["1 Ball-Pen", "2 Parker Pen"],
+      quantity: 2,
+      orderDate: "10/11/2025",
+      status: "Processing",
+    },
+  ];
+  const columns = [
+    { header: "order Id", field: "orderId" },
+    { header: "user Name", field: "userName" },
+    { header: "user Role", field: "userRole" },
+    {
+      header: "Items",
+      field: "items",
+      render: (row) => row.items.join(", "),
+    },
+    { header: "quantity", field: "quantity" },
+    { header: "order Date", field: "orderDate" },
+    { header: "status", field: "status" },
+    { header: "Action", field: "action" },
+  ];
+
+  return (
+    <Box sx={{ background: "white", padding: 2 }}>
+      <BreadCrumb linkText={[{ text: "Order Management" }]} />
+      <PagePath2 title="Order Management" />
+
+      <Box>
+        <DataTable
+          columns={columns}
+          data={data}
+          currentPage={1}
+          usersPerPage={5}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default OrderManagement;
